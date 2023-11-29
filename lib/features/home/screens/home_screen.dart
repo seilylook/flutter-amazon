7dd772c1d49a5676b2488b6d51dd21ce4adc39f6
seilylook <seilylook@naver.com>
2023-11-29 17:11:30 +0900

    feat: carousel slider

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/home/widgets/address_box.dart';
+import 'package:amazon_clone/features/home/widgets/carousel_image.dart';
 import 'package:amazon_clone/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 
@@ -95,6 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
           AddressBox(),
           SizedBox(height: 10),
           TopCategories(),
+          CarouselImage(),
         ],
       ),
     );
