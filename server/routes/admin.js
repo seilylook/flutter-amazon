75158ac417f05844bffc40028ff7f74918980836
seilylook <seilylook@naver.com>
2023-12-05 19:36:25 +0900

    debug: image upload success

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,20 +3,22 @@ const adminRouter = express.Router();
 const admin = require('../middleware/admin');
 const Product = require('../models/product');
 
-adminRouter.post('/admin/add-product', admin, async (req, res, next) => {
+adminRouter.post("/admin/add-product", admin, async (req, res) => {
     try {
-        const { name, description, quantity, images, category, price } = req.body;
+        const { name, description, images, quantity, price, category } = req.body;
         let product = new Product({
-            name, description, quantity, images, category, price
+            name,
+            description,
+            images,
+            quantity,
+            price,
+            category,
         });
-
         product = await product.save();
-
         res.json(product);
-
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
-})
+});
 
 module.exports = adminRouter;
